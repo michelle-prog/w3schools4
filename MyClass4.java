c7a573f237eb725300a566a46ca408cbd56732cc
michelle-prog <michellemawuena@gmail.com>
2019-06-18 17:49:57 +0000

    Fifth work

---
+++ MyClass4.java
@@ -0,0 +1,16 @@
+public class MyClass4{
+	static void myStaticMethod(){
+	System.out.println("Static methods can be called without creating objects");
+	}
+
+	public void myPublicMethod(){
+	System.out.println("Public methods must be called by creating objects");
+	}
+
+	public static void main(String[] args){
+	myStaticMethod();
+
+	MyClass myObj = new MyClass();
+	myObj.myPublicMethod();
+	}
+}
