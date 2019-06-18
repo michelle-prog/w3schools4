da2de51e13b907a56d5c8a71650f22aa0620e7f2
michelle-prog <michellemawuena@gmail.com>
2019-06-18 17:17:46 +0000

    Fourth work

---
+++ Person.java
@@ -0,0 +1,11 @@
+public class Person{
+	String frame = "John";
+	String Iname = "Doe";
+	int age = 24;
+
+	public static void main(String[] args){
+	Person myObj = new Person();
+	System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+	System.out.println("Age: " myObj.age);
+	}
+}
