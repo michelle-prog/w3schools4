764a7872843a310505c6aa6ddaab4a4fb70f405a
michelle-prog <michellemawuena@gmail.com>
2019-06-19 00:19:34 +0000

    Ninth work

---
+++ MyClass6.java
@@ -0,0 +1,12 @@
+public class MyClass6{
+	int x;
+
+	public MyClass6(int y){
+	x = y;
+	}
+
+	public static void main(String[] args){
+	MyClass6 myObj = new MyClass6(5);
+	System.out.println(myObj.x);
+	}
+}
