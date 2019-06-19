488d4a7e5c2b049db09c393959231d2f5cd18b76
michelle-prog <michellemawuena@gmail.com>
2019-06-19 00:13:13 +0000

    Eight work

---
+++ MyClass5.java
@@ -0,0 +1,12 @@
+public class MyClass5{
+	int x;
+
+	public MyClass5(){
+	x = 5;
+	}
+
+	public static void main(String[] args){
+	MyClass5 myObj = new MyClass5();
+	System.out.println(myObj.x);
+	}
+}
