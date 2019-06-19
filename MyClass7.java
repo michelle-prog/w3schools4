12816f4ce9561fa3f45c26ff79cd2d8926097edf
michelle-prog <michellemawuena@gmail.com>
2019-06-19 00:36:03 +0000

    Eleventh work

---
+++ MyClass7.java
@@ -0,0 +1,11 @@
+public class MyClass7{
+	final int x = 10;
+	final double PI = 3.14;
+
+	public static void main(String[] args){
+	MyClass7 myObj = new MyClass7();
+	myObj.x = 50;
+	myObj.PI = 25;
+	System.out.println(myObj.x);
+	}
+}
